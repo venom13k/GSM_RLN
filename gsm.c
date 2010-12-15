69e3d569f8420d7b850033d2161633795d59f329
Porotikov Stanislav <venom13k@yandex.ru>
2010-12-15 21:55:51 +0500

    Добавил размер окна по умолчанию..... вот))))

--- gsm.c
+++ gsm.c
@@ -137,12 +137,15 @@ int main( int   argc,
 	GtkWidget *scrolled;
 	GtkWidget *hbox;
 
+	gint width = 800;
+	gint height = 400;
+
 	n   = 0;
 	gtk_init (&argc, &argv);
 
 	window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
 	gtk_window_set_title (GTK_WINDOW (window), "GUI_RLN");
-
+	gtk_window_set_default_size (GTK_WINDOW (window), width, height);
 	g_signal_connect (window, "delete-event",
 		    G_CALLBACK (delete_event), NULL);
 
